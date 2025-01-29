9abacffc2fe1c50e2136a23019fd1c612665c17c
jiawei-zhang-a <jz4721@nyu.edu>
2025-01-29 07:46:14 -0600

    MAINT

--- Simulation/CalculatePowerForSingleOutcome.py
+++ Simulation/CalculatePowerForSingleOutcome.py
@@ -78,11 +78,10 @@ def report_delta():
 def run(Nsize, filepath, verbose=1, small_size = True):
 
     if beta_coef == 0.0:
-        Iter = 10000
+        Iter = 1000
     else:
         Iter = 1000
 
-    Iter = 100
     
     report_delta()
 
